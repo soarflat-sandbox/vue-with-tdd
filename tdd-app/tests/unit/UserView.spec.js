204ea0038a04ec96fc4cbf5fd1b68067e07233e8
soarflat <soarflat777@gmail.com>
2019-05-09 23:23:48 +0900

    refactor: 共通コードをまとめた関数を追加し、重複したテストコードを削除した。

--- tdd-app/tests/unit/UserView.spec.js
+++ tdd-app/tests/unit/UserView.spec.js
@@ -4,22 +4,32 @@ import VUserSearchForm from "@/components/VUserSearchForm";
 import VUserProfile from "@/components/VUserProfile";
 
 describe("UserView", () => {
-  it("コンポーネントがレンダリングされる", () => {
+  const build = () => {
     const wrapper = shallowMount(UserView);
+
+    return {
+      wrapper,
+      // VUserSearchFormにマッチする`Wrapper`を返す
+      // `wrapper.find`が即時で実行されるのを防ぐため、セレクタを取得する関数にする
+      userSearchForm: () => wrapper.find(VUserSearchForm),
+      // VUserProfileにマッチする`Wrapper`を返す
+      // `wrapper.find`が即時で実行されるのを防ぐため、セレクタを取得する関数にする
+      userProfile: () => wrapper.find(VUserProfile)
+    };
+  };
+
+  it("コンポーネントがレンダリングされる", () => {
+    const { wrapper } = build();
 
     // __snapshots__にhtmlのスナップショットが出力される
     expect(wrapper.html()).toMatchSnapshot();
   });
 
   it("子コンポーネントがレンダリングされる", () => {
-    const wrapper = shallowMount(UserView);
-    // VUserSearchFormにマッチする`Wrapper`を返す
-    const userSearchForm = wrapper.find(VUserSearchForm);
-    // VUserProfileにマッチする`Wrapper`を返す
-    const userProfile = wrapper.find(VUserProfile);
+    const { userSearchForm, userProfile } = build();
 
     // `Wrapper`（今回の場合、VUserSearchFormとVUserProfile）が存在すれば`true`を返す
-    expect(userSearchForm.exists()).toBe(true);
-    expect(userProfile.exists()).toBe(true);
+    expect(userSearchForm().exists()).toBe(true);
+    expect(userProfile().exists()).toBe(true);
   });
 });
